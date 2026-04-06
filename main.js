5ddfa15bfc9ecc7b817c6a2737be1a57c1c37a2a
nia-sempai <vladimir.serg@bk.ru>
2026-04-06 13:34:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const WORK_HOURS = 8;
-const SLEEP_TIME = WORK_HOURS * 60 * 60 * 1000;
-
 while (true) {
     mcp.readJiraTasks();
-    mcp.autoApproveMRs();
     writeCode();
     drinkCoffee();
-    // TODO: after mvp
     // takeOverTheWorld(); 
-    await new Promise(resolve => setTimeout(resolve, SLEEP_TIME));
 }
